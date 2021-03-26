8d21c5917161b12613c25a7ad7af3c36cc354348
Brandon Chaing <bchaing@ucsd.edu>
2021-03-26 00:27:54 -0700

    Added image uploads

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
-from flask import render_template
+import os
+from flask import render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from app import app
 
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
+
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_files():
+    for uploaded_file in request.files.getlist('file'):
+        filename = secure_filename(uploaded_file.filename)
+        if filename != '':
+            file_ext = os.path.splitext(filename)[1]
+            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                abort(400)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
