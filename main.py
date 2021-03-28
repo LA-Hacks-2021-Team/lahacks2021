42f590c2695a14aa3329f6c7632d9c56908b4c2f
Eldrin Pile <justeldrin@gmail.com>
2021-03-27 22:30:40 -0700

    Gap

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r'lahacks.json'
 
-    file_path = 'Crosshairs/L10_T2_G0.png'
+    file_path = 'Crosshairs/L20_T4_G2.png'
     project_id = "fleet-point-308504"
     model_id = "IOD4595987191406002176"
 
@@ -63,16 +63,16 @@ if __name__ == '__main__':
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     cv.imshow('test', gray)
     # blurs image
-    # blur = cv.GaussianBlur(gray, (1, 1), cv.BORDER_DEFAULT)
+    blur = cv.GaussianBlur(gray, (3, 3), cv.BORDER_DEFAULT)
     # detects canny edges and refines using dilation and erosion
-    canny = cv.Canny(gray, 0, 20)
+    canny = cv.Canny(blur, 0, 20)
     dilated = cv.dilate(canny, (1, 1), iterations=1)
     eroded = cv.erode(dilated, (1, 1), iterations=1)
     # detects contours and draws them in green onto blank canvas
     contours, hierarchies = cv.findContours(
         canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
     cv.drawContours(blank, contours, -1, (0, 255, 0), 1)
-    cnt = contours[0]
+    cnt = contours[3]
     # defines bounding rectangle around crosshair and prints width and height of box
     x, y, w, h = cv.boundingRect(cnt)
     print('Width = ' + str(w) + ' pixels')
@@ -87,8 +87,8 @@ if __name__ == '__main__':
     im = Image.open('Crosshairs/c1_cropped.png')
     rgb_im = im.convert('RGB')
     r, g, b = rgb_im.getpixel(((x + math.ceil(w/2)),(y + math.ceil(h/2))))
-    thickness = 0.5 + ((w - 3) * 0.5)
-    size = math.ceil(h / 2)
+    thickness = 2 + ((w - 5) * 0.5)
+    size = math.floor(h / 2)
     
     #Gap Calculator
     #Finds pixel distance between centers of hairs
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     dy = cyR - cyL
     D = np.sqrt(dx*dx+dy*dy)
     #Thickness and size roughly increase pixel distance by 2, 2.4 respectively, each addition of gap increases distance by 2
-    gap = round(((D - thickness*2 - size*2.4)/2 + (D - thickness*2 - size*2.4)%2),2)
+    gap = round(((D - thickness*2 - size*2.4)/2 + (D - thickness*2 - size*2.4)%2),0)
 
     print("cl_crosshaircolor 5; cl_crosshaircolor_b " + str(b) + "; cl_crosshaircolor_r " + str(r) + "; cl_crosshaircolor_g " + str(g) + "; cl_crosshairsize " + str(size)  + "; cl_crosshairthickness " + str(thickness) + "; cl_crosshairgap " + str(gap))
     cv.waitKey(0)
